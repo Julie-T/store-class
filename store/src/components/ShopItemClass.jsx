354944b32c348e598786a2ffb16c4b841ebc4ea7
Juliya <yula_sakura@list.ru>
2022-10-30 04:10:23 +0300

    Commit

---
+++ store/src/components/ShopItemClass.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+class ShopItem extends React.Component {
+    static propTypes = {
+        item: PropTypes.instanceOf().isRequired
+    }
+
+    render() {
+        const {item} = this.props;
+        return ( <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.price + ' ' + item.currency}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+        )
+    }
+}
+
+ShopItem.propTypes = {
+    brand: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    descriptionFull: PropTypes.string,
+    price: PropTypes.number,
+    currency: PropTypes.string,
+}
+
+export default ShopItem;
